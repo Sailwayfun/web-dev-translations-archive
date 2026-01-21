6cad5cf1353efa3244109d248dd376dfd81a7465
sail <sail@chainsecurity.asia>
2026-01-21 14:26:13 +0800

    chore: run format and lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
   integrations: [mdx()],
